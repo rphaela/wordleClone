b497ed1cf6cc3280f76383cec5dca931bdc28e15
Raphaela Oliveira <rphaelaoliveira@gmail.com>
2022-08-24 17:10:26 -0300

    instruções + add lista de palavras

--- js/main.js
+++ js/main.js
@@ -2,11 +2,27 @@ const tileDisplay = document.querySelector(".tile-container");
 const keyboard = document.querySelector(".key-container");
 const messageDisplay = document.querySelector(".message-container");
 
-
-const words = ['sagaz', 'amago', 'exito', 'mexer','termo', 'clone', 'nobre',
-'senso', 'algoz', 'afeto', 'plena', 'etica', 'mutua', 'tenue', 'sutil', 'vigor',
-'aquem', 
+const words = [
+  "sagaz",
+  "amago",
+  "exito",
+  "mexer",
+  "termo",
+  "clone",
+  "nobre",
+  "senso",
+  "algoz",
+  "afeto",
+  "plena",
+  "etica",
+  "mutua",
+  "tenue",
+  "sutil",
+  "vigor",
+  "aquem",
 ];
+
+const validWord = wordList.map((palavra) => palavra.toLowerCase());
 
 const keys = [
   "q",
@@ -55,7 +71,6 @@ let isGameOver = false;
 let wordle = words[currentWordIndex];
 let checkingRow = false;
 
-
 guessRows.forEach((guessRow, guessRowIndex) => {
   const rowElement = document.createElement("div");
   rowElement.setAttribute("id", "guessRow-" + guessRowIndex);
@@ -78,116 +93,116 @@ keys.forEach((key) => {
   keyboard.append(buttonElement);
 });
 
-  const handleClick = () => {
-    const clicks = document.querySelectorAll(".key-container button");
-    for (let i = 0; i < clicks.length; i++) {
-      clicks[i].addEventListener("click", ({target}) => {
-        const letter = target.getAttribute("id")
-            console.log("clicked", letter);
+const handleClick = () => {
+  const clicks = document.querySelectorAll(".key-container button");
+  for (let i = 0; i < clicks.length; i++) {
+    clicks[i].addEventListener("click", ({ target }) => {
+      const letter = target.getAttribute("id");
+      console.log("clicked", letter);
 
-        if (letter === "<<") {
-          deleteLetter();
-          // console.log("guessRows", guessRows);
-          return;
-        }
-        if (letter === "Enter") {
-          checkRow();
-          // console.log("guessRows", guessRows);
-          return;
-        }
-        addLetter(letter);
-
-      })
-    }
+      if (letter === "<<") {
+        deleteLetter();
+        // console.log("guessRows", guessRows);
+        return;
+      }
+      if (letter === "Enter") {
+        checkRow();
+        // console.log("guessRows", guessRows);
+        return;
+      }
+      addLetter(letter);
+    });
   }
+};
 
-  const handleKeypress = () => {
+const handleKeypress = () => {
   document.addEventListener("keydown", (e) => {
-      const letter = e.key
-          console.log("clicked", e.key);
-        
-          const lettersPattern = /[a-z]/;
-          lettersPattern.test(letter);
-        
-          if (letter === "Enter") {
-            checkRow();
-            console.log("guessRows", guessRows);
-            return;
-          }
-        
-          if (letter === "Backspace") {
-            deleteLetter();
-            console.log("guessRows", guessRows);
-            return;
-          } else {
-            if (lettersPattern.test(letter) && letter.length === 1) {
-              addLetter(letter);
-              return;
-            }
-          }
+    const letter = e.key;
+    console.log("clicked", e.key);
 
-    })
-  }
+    const lettersPattern = /[a-z]/;
+    lettersPattern.test(letter);
+
+    if (letter === "Enter") {
+      checkRow();
+      console.log("guessRows", guessRows);
+      return;
+    }
+
+    if (letter === "Backspace") {
+      deleteLetter();
+      console.log("guessRows", guessRows);
+      return;
+    } else {
+      if (lettersPattern.test(letter) && letter.length === 1) {
+        addLetter(letter);
+        return;
+      }
+    }
+  });
+};
 
 const showHelp = () => {
-    const help = new bootstrap.Modal(document.getElementById('howToPlay'), {});
-    help.show();
-}
+  const help = new bootstrap.Modal(document.getElementById("howToPlay"), {});
+  help.show();
+};
 
 const initLocalStorage = () => {
-    const storedCurrentWordIndex = window.localStorage.getItem('currentWordIndex')
-    if(!storedCurrentWordIndex) {
-        window.localStorage.setItem('currentWordIndex', currentWordIndex)
-        showHelp()
-    } else {
-    currentWordIndex = Number(storedCurrentWordIndex)
-    wordle = words[currentWordIndex]
-    }
-}
+  const storedCurrentWordIndex =
+    window.localStorage.getItem("currentWordIndex");
+  if (!storedCurrentWordIndex) {
+    window.localStorage.setItem("currentWordIndex", currentWordIndex);
+    showHelp();
+  } else {
+    currentWordIndex = Number(storedCurrentWordIndex);
+    wordle = words[currentWordIndex];
+  }
+};
 
-  initLocalStorage();
-  handleClick()
-  handleKeypress()
+initLocalStorage();
+handleClick();
+handleKeypress();
 
 const loadLocalStorage = () => {
-  currentWordIndex = Number(window.localStorage.getItem('currentWordIndex')) || currentWordIndex
-  currentRow = Number(window.localStorage.getItem('currentRow')) || currentRow
+  currentWordIndex =
+    Number(window.localStorage.getItem("currentWordIndex")) || currentWordIndex;
+  currentRow = Number(window.localStorage.getItem("currentRow")) || currentRow;
 
-  wordle = words[currentWordIndex]
+  wordle = words[currentWordIndex];
 
-  const storedBoardContainer = window.localStorage.getItem('boardContainer')
-  if(storedBoardContainer) {
-    document.querySelector(".tile-container").innerHTML = storedBoardContainer
+  const storedBoardContainer = window.localStorage.getItem("boardContainer");
+  if (storedBoardContainer) {
+    document.querySelector(".tile-container").innerHTML = storedBoardContainer;
   }
 
-  const storedKeyboardContainer = window.localStorage.getItem('keyboardContainer')
-  if(storedKeyboardContainer) {
-    document.querySelector(".key-container").innerHTML = storedKeyboardContainer
-    handleClick()
+  const storedKeyboardContainer =
+    window.localStorage.getItem("keyboardContainer");
+  if (storedKeyboardContainer) {
+    document.querySelector(".key-container").innerHTML =
+      storedKeyboardContainer;
+    handleClick();
   }
-}
+};
 
-loadLocalStorage()
+loadLocalStorage();
 
 const updateWordIndex = () => {
-    window.localStorage.setItem('currentWordIndex', currentWordIndex + 1)
-}
+  window.localStorage.setItem("currentWordIndex", currentWordIndex + 1);
+};
 
 const preserveGameState = () => {
-
-  const boardContainer = document.querySelector(".tile-container") 
-  window.localStorage.setItem('boardContainer', boardContainer.innerHTML)
+  const boardContainer = document.querySelector(".tile-container");
+  window.localStorage.setItem("boardContainer", boardContainer.innerHTML);
 
   const keyboardContainer = document.querySelector(".key-container");
-  window.localStorage.setItem('keyboardContainer', keyboardContainer.innerHTML)
-
-}
+  window.localStorage.setItem("keyboardContainer", keyboardContainer.innerHTML);
+};
 
 const resetGame = () => {
   window.localStorage.removeItem("boardContainer");
   window.localStorage.removeItem("keyboardContainer");
   window.localStorage.removeItem("currentRow");
-}
+};
 
 const addLetter = (letter) => {
   if (currentTile < 5 && currentRow < 6) {
@@ -217,118 +232,122 @@ const deleteLetter = () => {
 const checkRow = () => {
   const guess = guessRows[currentRow].join("");
 
-  if (currentTile > 4 && !checkingRow) {
-    console.log("guess is", guess + " wordle is " + wordle);
-    flipTile();
-    checkingRow = true
-    // localStorage.setItem('currentTile', currentTile)
+  if (!validWord.includes(guess)) {
+    showMessage("essa palavra não é aceita");
+    shakeTile();
+    return;
+  } else {
+    if (currentTile > 4 && !checkingRow) {
+      console.log("guess is", guess + " wordle is " + wordle);
+      flipTile();
+      checkingRow = true;
+      // localStorage.setItem('currentTile', currentTile)
 
-    if (wordle === guess) {
+      if (wordle === guess) {
+        const totalWins = window.localStorage.getItem("totalWins") || 0;
+        window.localStorage.setItem("totalWins", Number(totalWins) + 1);
 
-      const totalWins = window.localStorage.getItem("totalWins") || 0;
-      window.localStorage.setItem("totalWins", Number(totalWins) + 1);
+        const currentStreak = window.localStorage.getItem("currentStreak") || 0;
+        window.localStorage.setItem("currentStreak", Number(currentStreak) + 1);
+        const maxStreak = window.localStorage.getItem("maxStreak") || 0;
+        if (currentStreak >= maxStreak) {
+          window.localStorage.setItem("maxStreak", Number(maxStreak) + 1);
+        }
 
-      const currentStreak = window.localStorage.getItem("currentStreak") || 0;
-      window.localStorage.setItem("currentStreak", Number(currentStreak) + 1);
-      const maxStreak = window.localStorage.getItem("maxStreak") || 0;
-      if(currentStreak >= maxStreak) {
-        window.localStorage.setItem("maxStreak", Number(maxStreak) + 1);
-      }
+        updateTotal();
+        updateStats();
+        showStats();
 
-      updateTotal()
-      updateStats()
-      showStats()
+        if (currentRow === 0) {
+          setTimeout(() => {
+            showMessage("genius");
+            isGameOver = true;
+            jumpTile();
+            resetGame();
+          }, 2400);
+          updateWordIndex();
 
-      if (currentRow === 0) {
-        setTimeout(() => {
-          showMessage("genius");
-          isGameOver = true;
-          jumpTile();
-          resetGame();
-        }, 2400);
-        updateWordIndex()
-        
-        return;
-      }
-      if (currentRow === 1) {
-        setTimeout(() => {
-          showMessage("magnificent");
-          isGameOver = true;
-          jumpTile();
-          resetGame();
-        }, 2400);
-        updateWordIndex()
-        return;
-      }
-      if (currentRow === 2) {
-        setTimeout(() => {
-          showMessage("impressive");
-          isGameOver = true;
-          jumpTile();
-          resetGame();
-        }, 2400);
-        updateWordIndex()
-        return;
-      }
-      if (currentRow === 3) {
-        setTimeout(() => {
-          showMessage("splendid");
-          isGameOver = true;
-          jumpTile();
-          resetGame();
-        }, 2400);
-        updateWordIndex()
-        return;
-      }
-      if (currentRow === 4) {
-        setTimeout(() => {
-          showMessage("great");
-          isGameOver = true;
-          jumpTile();
-          resetGame();
-        }, 2400);
-        updateWordIndex()
-        return;
-      }
-      if (currentRow === 5) {
-        setTimeout(() => {
-          showMessage("phew");
-          isGameOver = true;
-          jumpTile();
-          resetGame();
-        }, 2400);
-        updateWordIndex()
-        return;
+          return;
+        }
+        if (currentRow === 1) {
+          setTimeout(() => {
+            showMessage("magnificent");
+            isGameOver = true;
+            jumpTile();
+            resetGame();
+          }, 2400);
+          updateWordIndex();
+          return;
+        }
+        if (currentRow === 2) {
+          setTimeout(() => {
+            showMessage("impressive");
+            isGameOver = true;
+            jumpTile();
+            resetGame();
+          }, 2400);
+          updateWordIndex();
+          return;
+        }
+        if (currentRow === 3) {
+          setTimeout(() => {
+            showMessage("splendid");
+            isGameOver = true;
+            jumpTile();
+            resetGame();
+          }, 2400);
+          updateWordIndex();
+          return;
+        }
+        if (currentRow === 4) {
+          setTimeout(() => {
+            showMessage("great");
+            isGameOver = true;
+            jumpTile();
+            resetGame();
+          }, 2400);
+          updateWordIndex();
+          return;
+        }
+        if (currentRow === 5) {
+          setTimeout(() => {
+            showMessage("phew");
+            isGameOver = true;
+            jumpTile();
+            resetGame();
+          }, 2400);
+          updateWordIndex();
+          return;
+        }
+      } else {
+        if (currentRow >= 5) {
+          setTimeout(() => {
+            showMessage(`${wordle}`);
+            isGameOver = true;
+            resetGame();
+          }, 2400);
+          updateWordIndex();
+          window.localStorage.setItem("currentStreak", 0);
+          updateTotal();
+          updateStats();
+          showStats();
+          return;
+        }
+        if (currentRow < 5) {
+          setTimeout(() => {
+            currentRow++;
+            window.localStorage.setItem("currentRow", currentRow);
+            currentTile = 0;
+            checkingRow = false;
+          }, 2400);
+        }
       }
     } else {
-      if (currentRow >= 5) {
-        setTimeout(() => {
-          showMessage(`${wordle}`);
-          isGameOver = true;
-          resetGame()
-        }, 2400);
-        updateWordIndex()
-        window.localStorage.setItem("currentStreak", 0)
-        updateTotal()
-        updateStats()
-        showStats()
+      if (!checkingRow) {
+        showMessage("Not enough letters");
+        shakeTile();
         return;
       }
-      if (currentRow < 5) {
-        setTimeout(() => {
-        currentRow++;
-        window.localStorage.setItem('currentRow', currentRow)
-        currentTile = 0;
-        checkingRow = false;
-        }, 2400)
-        
-      }
-    }
-  } else {
-    if(!checkingRow) {
-    showMessage("Not enough letters");
-    shakeTile()
-    return;
     }
   }
 };
@@ -340,12 +359,10 @@ const showMessage = (message) => {
   setTimeout(() => messageDisplay.removeChild(messageElement), 2000);
 };
 
-
 const addColorToKey = (keyLetter, color) => {
   const key = document.getElementById(keyLetter);
   key.classList.add(color);
 };
-
 
 const flipTile = () => {
   const rowTiles = document.querySelector("#guessRow-" + currentRow).childNodes;
@@ -376,9 +393,9 @@ const flipTile = () => {
       tile.classList.add(guess[index].color);
       addColorToKey(guess[index].letter, guess[index].color);
 
-    if (index === 4) {
-        preserveGameState()
-    }
+      if (index === 4) {
+        preserveGameState();
+      }
     }, 500 * index);
   });
 };
@@ -392,43 +409,37 @@ const shakeTile = () => {
 };
 
 const jumpTile = () => {
-    const rowTiles = document.querySelector("#guessRow-" + currentRow).childNodes;
-    rowTiles.forEach((tile, index) => {
-        setTimeout(() => {
-          tile.classList.add("jump");
-        }, 250 * index);
-      });
+  const rowTiles = document.querySelector("#guessRow-" + currentRow).childNodes;
+  rowTiles.forEach((tile, index) => {
+    setTimeout(() => {
+      tile.classList.add("jump");
+    }, 250 * index);
+  });
 };
 
 const updateTotal = () => {
-  const totalGames = window.localStorage.getItem("totalGames") || 0
-  window.localStorage.setItem("totalGames", Number(totalGames) + 1)
-}
+  const totalGames = window.localStorage.getItem("totalGames") || 0;
+  window.localStorage.setItem("totalGames", Number(totalGames) + 1);
+};
 
 const updateStats = () => {
-  const currentStreak = window.localStorage.getItem("currentStreak") || 0
-  const totalGames = window.localStorage.getItem("totalGames") || 0
-  const totalWins = window.localStorage.getItem("totalWins") 
-  const maxStreak = window.localStorage.getItem("maxStreak") || 0
+  const currentStreak = window.localStorage.getItem("currentStreak") || 0;
+  const totalGames = window.localStorage.getItem("totalGames") || 0;
+  const totalWins = window.localStorage.getItem("totalWins");
+  const maxStreak = window.localStorage.getItem("maxStreak") || 0;
 
   document.getElementById("totalPlayed").textContent = totalGames;
   document.getElementById("currentStreak").textContent = currentStreak;
-  document.getElementById("bestStreak").textContent = maxStreak
+  document.getElementById("bestStreak").textContent = maxStreak;
 
   const winPct = Math.round((totalWins / totalGames) * 100) || 0;
-  document.getElementById("winPct").textContent = winPct
-  
-}
-updateStats()
+  document.getElementById("winPct").textContent = winPct;
+};
+updateStats();
 
 const showStats = () => {
   setTimeout(() => {
-    const stats = new bootstrap.Modal(document.getElementById('stats'), {});
+    const stats = new bootstrap.Modal(document.getElementById("stats"), {});
     stats.show();
-  }, 3500)
-}
-
-
-
-
-
+  }, 3500);
+};
